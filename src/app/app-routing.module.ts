efa93c1209652eb961df5e190bf94a5f76564e87
Yandralal Thakre <Yandralal.Thakre@wolterskluwer.com>
2024-01-09 16:39:00 +0530

    Added accordian control

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccordionComponent } from './examples/accordion/accordion.component';
 
-const routes: Routes = [];
+const routes: Routes = [ { path: 'accord', component: AccordionComponent } ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JumpstartComponentsModule } from '@wk/components-angular15';
+import { AccordionComponent } from './examples/accordion/accordion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionComponent
   ],
   imports: [
     BrowserModule,
